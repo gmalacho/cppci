0a63b7ae8654f0f7fdea4faa95515f0819c70b99
gmalacho <39593690+gmalacho@users.noreply.github.com>
2024-12-26 17:00:35 +0000

    New version.

--- .devcontainer/update-gcc.sh
+++ .devcontainer/update-gcc.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Updateing gcc"
+echo "Updating gcc"
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test # Ignore if not ubuntu
+# Add the PPA repository silently
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # Ignore if not Ubuntu
 
-sudo apt-get update
+# Update package lists
+sudo apt-get update -y
 
-sudo apt-get install gcc-4.9
+# Install gcc-4.9 without user interaction
+sudo apt-get install -y gcc
 
-sudo apt-get upgrade libstdc++6
+# Upgrade libstdc++6 without user interaction
+sudo apt-get upgrade -y libstdc++6
 
 echo "Update gcc done"
