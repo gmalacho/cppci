11e3e1cb73d32b77a3384c1de73963e218e17ff1
gmalacho <39593690+gmalacho@users.noreply.github.com>
2024-12-26 20:00:52 +0000

    New CI job.

--- ut/run_ut.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <iostream>
-
-int main(int argc, char **argv) {
-    std::cout << "Starting tests in file: " << __FILE__ << std::endl;
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
